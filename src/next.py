a1bd3973b38871a93d37916c9b446167c1025471
Sil Westerveld <sil@madx.nl>
2021-05-25 15:25:31 +0200

    Add GitPython for access to git repos

--- src/next.py
+++ src/next.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import git
 import json
 import os
 
@@ -17,22 +18,23 @@ class RepositoryManager:
         self.repository = repository
 
     def get_repo_dir(self):
-        path = self.configuration["repositories"][self.repository]["path"]
-        full_path = None
+        full_path = path = self.configuration["repositories"][self.repository]["path"]
 
+        # make sure to use an absolute path
         if path[0] == ".":
-            # relative path
-            # TODO: maybe don't accept this because relative path is difficult: it's relative to location of binary
-            full_path = os.path.normpath(f"{os.getcwd()}/{path}")
+            # when a relative path has been given
+            raise Exception("Sorry, no relative paths allowed")
         elif path[0] == "~":
-            # path in homedir
+            # when a path in homedir has been given
             homedir = os.path.expanduser(path.split("/")[0])
             sub_dirs = "/".join(path.split("/")[1:])
             full_path = os.path.join(homedir, sub_dirs)
 
-        return full_path or path
+        repo = git.Repo(full_path or path)
+        assert not repo.bare
+        return full_path
 
 
 if __name__ == '__main__':
     vm = RepositoryManager("backend")
-    print(f"backend: {vm.get_repo_dir()}")
+    print(f"repo: {vm.get_repo_dir()}")  # Just to prove the repo could be used
